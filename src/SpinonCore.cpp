b8a135611d5f580d72c6d05e648f902d2c328d21
wjxway <wjxway@163.com>
2023-02-18 22:22:22 +0000

    minor edit

--- src/SpinonCore.cpp
+++ src/SpinonCore.cpp
@@ -23,7 +23,7 @@ void loop()
 {
     Serial.print("\n----DATA START----\n");
 
-    for (int i = 0; i < 1000; i++)
+    for (size_t i = 0; i < 1000; i++)
     {
         auto res = IR::Sense::Transmit_and_sense(i);
 
